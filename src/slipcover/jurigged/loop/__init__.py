f2d6308deced1b78b32f253e764ede2aed292b28
FreddyYJ <kyj1411@unist.ac.kr>
2023-07-28 07:34:38 +0000

    Remove original decorators from jurigged

--- src/slipcover/jurigged/loop/__init__.py
+++ src/slipcover/jurigged/loop/__init__.py
@@ -10,60 +10,60 @@ from .repairloop import RepairloopRunner
 from .repairutils import prune_default_global_var,BugInformation
 
 
-def keyword_decorator(deco):
-    """Wrap a decorator to optionally takes keyword arguments."""
+# def keyword_decorator(deco):
+#     """Wrap a decorator to optionally takes keyword arguments."""
 
-    @functools.wraps(deco)
-    def new_deco(fn=None, **kwargs):
-        if fn is None:
+#     @functools.wraps(deco)
+#     def new_deco(fn=None, **kwargs):
+#         if fn is None:
 
-            @functools.wraps(deco)
-            def newer_deco(fn):
-                return deco(fn, **kwargs)
+#             @functools.wraps(deco)
+#             def newer_deco(fn):
+#                 return deco(fn, **kwargs)
 
-            return newer_deco
-        else:
-            return deco(fn, **kwargs)
+#             return newer_deco
+#         else:
+#             return deco(fn, **kwargs)
 
-    return new_deco
-
-
-@keyword_decorator
-def loop(fn, interface=None, only_on_error=False):
-    if interface is None:
-        try:
-            import rich
-
-            interface = "rich"
-        except ModuleNotFoundError:
-            interface = "basic"
-    # interface='basic'
-
-    if interface == "rich":
-        from .richloop import RichDeveloopRunner
-
-        interface = RichDeveloopRunner
-    elif interface == "basic":
-        interface = BasicDeveloopRunner
-    elif isinstance(interface, str):
-        raise Exception(f"Unknown develoop interface: '{interface}'")
-
-    interface=RepairloopRunner
-    # FIXME: Too large overhead at calling Develoop in target program
-    return Develoop(fn, on_error=only_on_error, runner_class=interface)
+#     return new_deco
 
 
-loop_on_error = functools.partial(loop, only_on_error=True)
-xloop = loop_on_error
+# @keyword_decorator
+# def loop(fn, interface=None, only_on_error=False):
+#     if interface is None:
+#         try:
+#             import rich
 
-__ = SimpleNamespace(
-    loop=loop,
-    loop_on_error=loop_on_error,
-    xloop=xloop,
-    give=give,
-    given=given,
-)
+#             interface = "rich"
+#         except ModuleNotFoundError:
+#             interface = "basic"
+#     # interface='basic'
+
+#     if interface == "rich":
+#         from .richloop import RichDeveloopRunner
+
+#         interface = RichDeveloopRunner
+#     elif interface == "basic":
+#         interface = BasicDeveloopRunner
+#     elif isinstance(interface, str):
+#         raise Exception(f"Unknown develoop interface: '{interface}'")
+
+#     interface=RepairloopRunner
+#     # FIXME: Too large overhead at calling Develoop in target program
+#     return Develoop(fn, on_error=only_on_error, runner_class=interface)
 
 
-def inject():
-    builtins.__ = __
+# loop_on_error = functools.partial(loop, only_on_error=True)
+# xloop = loop_on_error
+
+# __ = SimpleNamespace(
+#     loop=loop,
+#     loop_on_error=loop_on_error,
+#     xloop=xloop,
+#     give=give,
+#     given=given,
+# )
+
+
+# def inject():
+#     builtins.__ = __
