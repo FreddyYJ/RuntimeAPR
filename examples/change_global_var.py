198041cd521ea014c6f84fee8d092533b4609b62
FreddyYJ <kyj1411@unist.ac.kr>
2023-08-07 05:57:33 +0000

    Update example

--- examples/change_global_var.py
+++ examples/change_global_var.py
@@ -1,14 +1,21 @@
 # from slipcover.jurigged.loop import Develoop
 a=0
 
-def inc():
+def temp(a):
+    return a!=3
+
+def inc(b):
     # a+=1
     global a
     a=a+1
-    if a==3:
-        raise ValueError
-    else:
+    if temp(a):
         print(f'a: {a}')
+    else:
+        raise ValueError
+    return a,b
 
 for _ in range(5):
-    inc()
+    # try:
+        inc(a)
+    # except Exception as e:
+    #     raise
