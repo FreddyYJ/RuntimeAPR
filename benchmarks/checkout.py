54ad7c21b98073021722df0a6939b644357d0e81
FreddyYJ <kyj1411@unist.ac.kr>
2023-08-14 16:24:15 +0900

    Add checkout and test fixed version

--- benchmarks/checkout.py
+++ benchmarks/checkout.py
@@ -19,9 +19,28 @@ def checkout(subject:str,version:int):
     else:
         print(f'{subject}-{version} checkout success!')
 
+def checkout_fixed(subject:str,version:int):
+    print(f'Checking out {subject}-{version}f')
+    if not os.path.exists(f'/root/project/RuntimeAPR/benchmarks/{subject}'):
+        os.mkdir(f'/root/project/RuntimeAPR/benchmarks/{subject}')
+
+    result=subprocess.run(['bugsinpy-checkout','-p',subject,'-i',str(version),'-v','1',
+                           '-w',f'/root/project/RuntimeAPR/benchmarks/{subject}/{subject}-{version}f'],
+                           stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    
+    # Store the output in the checkout.log file
+    with open(f'/root/project/RuntimeAPR/benchmarks/{subject}/{subject}-{version}f/{subject}/checkout.log','w') as f:
+        f.write(result.stdout.decode())
+
+    if result.returncode!=0:
+        print(f'{subject}-{version}f checkout returns {result.returncode}')
+    else:
+        print(f'{subject}-{version}f checkout success!')
+
+
 import benchmark
 
 for sub in benchmark.SUBJECTS:
     for i in range(1,benchmark.BUGS_NUMBER[sub]+1):
         if (sub,i) not in benchmark.EXCEPT_BUGS:
-            checkout(sub,i)
+            checkout_fixed(sub,i)
--- benchmarks/test.py
+++ benchmarks/test.py
@@ -28,10 +28,40 @@ def test(subject:str,version:str):
     else:
         print(f'{subject}-{version} test success!')
         return True
+    
+def test_fixed(subject:str,version:str):
+    print(f'Compiling {subject}-{version}f')
+    result=subprocess.run(['bugsinpy-compile'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,
+                          cwd=f'/root/project/RuntimeAPR/benchmarks/{subject}/{subject}-{version}f/{subject}')
+    
+    # Store the output in the compile.log file
+    with open(f'/root/project/RuntimeAPR/benchmarks/{subject}/{subject}-{version}f/{subject}/compile.log','w') as f:
+        f.write(result.stdout.decode())
+
+    if result.returncode!=0:
+        print(f'{subject}-{version}f compile returns {result.returncode}')
+    else:
+        print(f'{subject}-{version}f compile success, running tests')
+
+    # Run tests
+    result=subprocess.run(['bugsinpy-test'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,
+                          cwd=f'/root/project/RuntimeAPR/benchmarks/{subject}/{subject}-{version}f/{subject}')
+    
+    # Store the output in the test.log file
+    with open(f'/root/project/RuntimeAPR/benchmarks/{subject}/{subject}-{version}f/{subject}/test.log','w') as f:
+        f.write(result.stdout.decode())
+
+    if result.returncode!=0:
+        print(f'{subject}-{version}f test returns {result.returncode}')
+        return False
+    else:
+        print(f'{subject}-{version}f test success!')
+        return True
+
 
 import benchmark
 
 for sub in benchmark.SUBJECTS:
     for i in range(1,benchmark.BUGS_NUMBER[sub]+1):
         if (sub,i) not in benchmark.EXCEPT_BUGS:
-            test(sub,i)
+            test_fixed(sub,i)
